62cda53113e604a5f7fc3203027dbcb8e86855ee
Hauke D <haukex@zero-g.net>
2025-12-27 14:56:49 +0100

    Rewrite; allow any Z85 lengths

--- arduino-ascon128-test.ino
+++ arduino-ascon128-test.ino
@@ -1,4 +1,57 @@
 
+#include <stdlib.h>
+#include <stdint.h>
+
+/* ********** ********** ********** ********** Z85 ********** ********** ********** ********** */
+
+static const uint8_t _z85_tbl[] = "0123456789abcdefghijklmnopqrstuvwxyz"
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ.-:+=^!/*?&<>()[]{}@%$#";
+
+/** Write a byte array to the specified stream using the Z85 encoding, with the extension
+ * that, as opposed to the specification, buffers of any length can be encoded.
+ *
+ * This implementation is tested against Python's `base64.z85encode` via `test.py` in this repository.
+ *
+ * References:
+ * - https://rfc.zeromq.org/spec/32/
+ * - https://github.com/zeromq/rfc/blob/master/src/spec_32.c
+ */
+void print_z85(Print &out, const uint8_t* buffer, const size_t len) {
+  for (size_t pos=0; pos<len; pos+=4) {
+    const uint8_t left = pos+4<len ? 4 : len-pos;
+    uint32_t n = __builtin_bswap32(*(uint32_t*)&buffer[pos]);  // Arduino is little-endian, Z85 uses big-endian
+                out.write(_z85_tbl[(n / 52200625) % 85]);
+                out.write(_z85_tbl[(n / 614125  ) % 85]);
+    if (left>1) out.write(_z85_tbl[(n / 7225    ) % 85]);
+    if (left>2) out.write(_z85_tbl[(n / 85      ) % 85]);
+    if (left>3) out.write(_z85_tbl[ n             % 85]);
+  }
+}
+
+/* ********** ********** z85_test ********** ********** */
+
+uint8_t hex_chr(uint8_t c) {
+  if (c >= '0' && c <= '9') return c - '0';
+  if (c >= 'a' && c <= 'f') return c - 'a' + 10;
+  if (c >= 'A' && c <= 'F') return c - 'A' + 10;
+  return 0;
+}
+
+void z85_test(const uint8_t* buffer, const size_t len) {
+  const size_t BUF_SZ = 16;
+  if (len%2 || len>BUF_SZ*2) {
+    Serial.println("Bad length");
+    return;
+  }
+  static uint8_t data[BUF_SZ];
+  for (size_t i=0; i<len; i+=2)
+    data[i/2] = (hex_chr(buffer[i]) << 4) | hex_chr(buffer[i+1]);
+  print_z85(Serial, data, len/2);
+  Serial.write('\n');
+}
+
+/* ********** ********** ********** ********** Ascon-128 ********** ********** ********** ********** */
+
 /* https://rweather.github.io/arduinolibs/classAscon128.html
  * Install "Crypto by Rhys Weatherley 0.4.0", though unfortunately that doesn't seem to include the following:
  * wget https://github.com/rweather/arduinolibs/raw/refs/heads/master/libraries/CryptoLW/src/Ascon128.cpp
@@ -9,49 +62,49 @@
  */
 #include "Ascon128.h"
 
-const uint8_t z85_tbl[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-:+=^!/*?&<>()[]{}@%$#";
-/** Encodes 32 bits into 5 ASCII bytes according to the Z85 specification.
- *
- * References:
- * - https://rfc.zeromq.org/spec/32/
- * - https://github.com/zeromq/rfc/blob/master/src/spec_32.c
- */
-void z85_enc(const uint8_t in[4], uint8_t out[5]) {
-  uint32_t n = __builtin_bswap32(*(uint32_t*)in);  // Arduino is little-endian, Z85 uses big-endian
-  for (uint8_t i=4; i>0; i--) { out[i] = z85_tbl[n % 85]; n /= 85; }
-  out[0] = z85_tbl[n % 85];  // save one division by pulling this out of the loop
-}
+/* ********** ********** IV ********** ********** */
 
-// Arduino is Little-Endian so this works:
-typedef struct {
-  uint64_t lsb;
-  uint64_t msb;
-} uint128_t;
-typedef union {
-  uint128_t i;
-  uint8_t b[16];
-} uint128buf_t;
+// Arduino is little-endian so this works:
+typedef struct { uint64_t lsb; uint64_t msb; } uint128_t;
+typedef union  { uint128_t i; uint8_t b[16]; } uint128buf_t;
 /* Arduino quirk: b/c uint128_t is used as a function argument here,
  * I need to include the function prototype after the typedef. */
-inline void inc_uint128(uint128_t &x);
-inline void inc_uint128(uint128_t &x) { if (!(++x.lsb)) x.msb++; }
+inline uint128_t& operator++(uint128_t& x);
+inline uint128_t& operator++(uint128_t& x) { if (!(++x.lsb)) x.msb++; return x; }
 
-/** Prints up to 16 bytes encoded into up to 20 Z85-encoded ASCII characters.
- * `sz` must be 16 or less; if it is less, then it is assumed the rest of the
- * buffer is padded properly, e.g. with NUL bytes. */
-void print_buf16_z85(uint8_t buf[16], size_t sz) {
-  if (!sz) return;
-  const size_t m = sz<16 ? (sz-1)/4+1 : 4;
-  static uint8_t out_buf[20];
-  for(size_t i=0; i<m; i++)
-    z85_enc(&buf[i*4], &out_buf[i*5]);
-  Serial.write(out_buf, m*5);
+/* ********** ********** crypt_test ********** ********** */
+
+void crypt_test(const uint8_t* buffer, const size_t len) {
+  static Ascon128 cipher = Ascon128();
+  static uint128buf_t iv = { .i = {0, 0} };
+  // note .setKey also resets internal state
+  cipher.setKey((const uint8_t*)"Super Secret! :)", 16);  // key size is always 16
+  // Note: In theory, could also use `const uint32_t m = millis()` for IV - wraps after ~49.7 days (!)
+  cipher.setIV(iv.b, 16);  // IV size is always 16
+  cipher.addAuthData(iv.b, 16);
+  // IV can be decoded in Python by `int.from_bytes(z85decode(buf[:20]), byteorder='little')`
+  print_z85(Serial, iv.b, 16);
+  ++iv.i;
+
+  // Encrypt in blocks of 16 bytes because then we can use the same buffer for the tag too.
+  const size_t CRYPT_BUF_SZ = 16;
+  static uint8_t crypt_buf[CRYPT_BUF_SZ];
+  for(size_t pos=0; pos<len; pos+=CRYPT_BUF_SZ) {
+    const uint8_t left = pos+CRYPT_BUF_SZ<len ? CRYPT_BUF_SZ : len-pos;
+    cipher.encrypt(crypt_buf, &buffer[pos], left);
+    print_z85(Serial, crypt_buf, left);
+  }
+  cipher.computeTag(crypt_buf, 16);  // tag size is always 16
+  print_z85(Serial, crypt_buf, 16);
+  cipher.clear();
+  Serial.write('\n');
 }
 
-const uint8_t KEY[] = "Super Secret! :)";  // must be 16 bytes
+/* ********** ********** ********** ********** Main ********** ********** ********** ********** */
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Ready");
 }
 
 void loop() {
@@ -59,39 +112,19 @@ void loop() {
 
   const size_t BUF_SZ = 128;
   static uint8_t buffer[BUF_SZ];
-  //memset(buffer, 0, BUF_SZ);
   // readBytesUntil does not include the \n in the resulting buffer.
   const size_t buf_len = Serial.readBytesUntil('\n', buffer, BUF_SZ-1);
   buffer[buf_len] = '\0';
 
-  static Ascon128 cipher;
-  static uint128buf_t iv = {0,0};
-  // note .setKey also resets internal state
-  cipher.setKey(KEY, 16);  // key size is always 16
-  // Note: In theory, could also use `const uint32_t m = millis()` for IV - wraps after ~50 days (!)
-  cipher.setIV(iv.b, 16);  // IV size is always 16
-  cipher.addAuthData(iv.b, 16);
-  // IV can be decoded in Python by `int.from_bytes(z85decode(buf[:20]), byteorder='little')`
-  print_buf16_z85(iv.b, 16);
-  inc_uint128(iv.i);
-
-  /* We'll encrypt in blocks of 16 bytes because that just makes it easier in that
-   * we can use one function to print the IV, Tag, and encrypted data, and it saves
-   * a bit of RAM because we only need one 16 byte and one 20 byte buffer. */
-  const size_t CRYPT_BUF_SZ = 16;
-  static uint8_t crypt_buf[CRYPT_BUF_SZ];
-  const size_t slen = strlen((const char*)buffer);  // is safe because we explicitly NUL-terminate above
-  for(size_t pos=0; pos<slen; pos+=CRYPT_BUF_SZ) {
-    const uint8_t sz = pos+CRYPT_BUF_SZ<slen ? CRYPT_BUF_SZ : slen-pos;
-    /* Pad data with NUL bytes that the receiver can strip - this obviously means that
-     * the data buffer shouldn't end on NUL bytes; which the `strlen` above guarantees. */
-    if (sz<CRYPT_BUF_SZ) memset(crypt_buf, 0, CRYPT_BUF_SZ);
-    cipher.encrypt(crypt_buf, &buffer[pos], sz);
-    print_buf16_z85(crypt_buf, sz);
+  switch(buffer[0]) {
+    case 'c':
+      crypt_test(buffer+1, buf_len-1);
+      break;
+    case 'z':
+      z85_test(buffer+1, buf_len-1);
+      break;
+    default:
+      Serial.println("Unrecognized command");
   }
-  cipher.computeTag(crypt_buf, 16);  // tag size is always 16
-  print_buf16_z85(crypt_buf, 16);
-  cipher.clear();
 
-  Serial.println();
 }
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+# pylint: disable=missing-function-docstring
 """Test of Ascon-128 on Arduino."""
-import time
-from base64 import z85decode
+from itertools import product
+from base64 import z85encode, z85decode
 # https://github.com/pyserial/pyserial
 import serial  # type: ignore[import-untyped]
 # https://github.com/meichlseder/pyascon
@@ -9,9 +10,9 @@ import ascon   # type: ignore[import-untyped]
 
 # spell: ignore baudrate
 
-KEY = b'Super Secret! :)'  # must be 16 bytes
+CRYPT_KEY = b'Super Secret! :)'  # must be 16 bytes
 
-TESTS = (
+CRYPT_TESTS = (
     b'a',
     b'ab',
     b'abc',
@@ -20,43 +21,72 @@ TESTS = (
     b'Hello, World! This is a test.',
     # spell: disable
     b'Lorem ipsum dolor sit amet, consectetur adipiscing elit sapien.',
-    b'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi'
-    b' magna, pulvinar non ipsum eget, finibus aliquet metus.',
+    # 126+\n+\0:
+    b'Lorem ipsum dolor sit amet, consectetur \xE4dipiscing elit. Pellentesque'
+    b' mi magna, pulvinar non \xEEpsum eget, finibus \xE6lquet metus.',
     # spell: enable
 )
 
-with serial.Serial(port='COM29', baudrate=115200, timeout=5) as ser:
-    print("Waiting for boot...")
-    time.sleep(2)  # not sure why this is required but it is (Arduino resets?)
-    seen_iv: set[bytes] = set()
-    for t in TESTS:
-        print(f"##### {t=} #####")
-        ser.write(t)
-        ser.write(b'\n')
+_prev_iv: int = -1
 
-        buf = ser.readline().rstrip(b'\r\n')
-        print(f" {buf=}")
 
-        iv = z85decode(buf[:20])
-        print(f"  {iv=} {int.from_bytes(iv, byteorder='little')}")
-        if iv in seen_iv:
-            raise RuntimeError("nonce seen twice")
-        seen_iv.add(iv)
-
-        # Note that the sender guarantees that the data doesn't contain
-        # NUL bytes, and pads the data to multiples of 4 using NUL bytes
-        # to fulfill the Z85 specification.
-        data = z85decode(buf[20:-20])
-        tag = z85decode(buf[-20:])
-        print(f"{data=} {tag=}")
-
-        enc = ascon.encrypt(KEY, iv, iv, t)
-        exp = enc[:-16]
-        exp_tag = enc[-16:]
-        print(f" {exp=} tag={exp_tag!r}")
-
-        out = ascon.decrypt(KEY, iv, iv, data.rstrip(b'\0')+tag)
-        print(f" {out=}")
-        if out != t:
+def do_crypt_test(ser: serial.Serial):
+    for t in CRYPT_TESTS:
+        # Send
+        ser.write(b'c' + t + b'\n')
+        # Receive
+        rx = ser.readline().rstrip(b'\r\n')
+        # Split
+        if len(rx) < 40:
+            raise ValueError(f"data too short: {rx!r}")
+        iv = z85decode(rx[:20])
+        got = z85decode(rx[20:-20])
+        tag = z85decode(rx[-20:])
+        # Check IV
+        ivn = int.from_bytes(iv, byteorder='little')
+        global _prev_iv  # pylint: disable=global-statement
+        if ivn <= _prev_iv:
+            raise ValueError(f"IV repeated: {iv!r}")
+        _prev_iv = ivn
+        # Decrypt
+        out = ascon.decrypt(CRYPT_KEY, iv, iv, got+tag)
+        if out != t:  # mismatch
+            print(f"##### {t!r} #####")
+            print(f" {rx=} iv={ivn!r}")
+            # Show expected encryption result
+            enc = ascon.encrypt(CRYPT_KEY, iv, iv, t)
+            exp = z85encode(iv) + z85encode(enc[:-16]) + z85encode(enc[-16:])
+            print(f"{exp=}")
+            print(f"    {enc[:-16]!r} tag={enc[-16:]!r}")
+            print(f"{got=} {tag=}")
+            print(f"{out=}")
             raise RuntimeError(f"{out=} != {t=}")
-        print("OK")
+        print(f"OK {t.decode('CP1252')} {rx.decode('ASCII')}")
+
+
+def do_z85_test(ser: serial.Serial):
+    for length in range(6):
+        for combo in product((0, 1, 128, 255), repeat=length):
+            t = bytes(combo)
+            # Send
+            ser.write(b'z' + t.hex().encode('ASCII') + b'\n')
+            # Receive
+            rx = ser.readline().rstrip(b'\r\n')
+            exp = z85encode(t)
+            if rx != exp:
+                raise RuntimeError(f"{rx=} != {exp=}")
+            print(f"OK {t.hex()} {rx.decode('ASCII')}")
+
+
+def main():
+    with serial.Serial(
+            port='COM29', baudrate=115200, timeout=5) as ser:
+        print("Waiting for boot...")
+        if not ser.read_until(b'Ready\r\n').endswith(b'Ready\r\n'):
+            raise RuntimeError('Failed to get "Ready" from Arduino')
+        do_z85_test(ser)
+        do_crypt_test(ser)
+
+
+if __name__ == '__main__':
+    main()
