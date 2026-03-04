f5b2cdef5d4785db059d0a2c9d1fcb240625aca2
Hauke D <haukex@zero-g.net>
2026-03-04 12:27:35 +0100

    Fix buffer overrun in z85_print

--- z85.cpp
+++ z85.cpp
@@ -15,8 +15,9 @@ static const uint8_t _z85_tbl[] = "0123456789abcdefghijklmnopqrstuvwxyz"
 void z85_print(Print &out, const uint8_t* buffer, const size_t len) {
   for (size_t pos=0; pos<len; pos+=4) {
     const uint8_t left = pos+4<len ? 4 : len-pos;
-    //TODO: if left<4, we need to mask the remaining bytes of `buffer`!
-    uint32_t n = __builtin_bswap32(*(uint32_t*)&buffer[pos]);  // Arduino is little-endian, Z85 uses big-endian
+    uint32_t n = 0;  // treat the rest of the bytes as 0!
+    memcpy(&n, &buffer[pos], left);
+    n = __builtin_bswap32(n);  // Arduino is little-endian, Z85 uses big-endian
                 out.write(_z85_tbl[(n / 52200625) % 85]);
                 out.write(_z85_tbl[(n / 614125  ) % 85]);
     if (left>1) out.write(_z85_tbl[(n / 7225    ) % 85]);
